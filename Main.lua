c05c8d240bb31bb38560971655df408c785062c0
Dev-BlueX <bluexscript11@gmail.com>
2025-03-06 19:05:51 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -5,7 +5,7 @@ if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 elseif placeId == 205224386 then
 	--HideAndSeek
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/HideAndSeek.lua"))()
-elseif placeId == 116495829188952 then
+elseif placeId == 116495829188952 or placeId == 70876832253163 then
     	--DeadRails
     	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/DeadRails.lua"))()
 end
