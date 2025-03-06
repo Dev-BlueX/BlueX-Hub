46ad8e898868f2070f59240d413cc00b832da26b
Dev-BlueX <bluexscript11@gmail.com>
2025-03-06 18:26:16 +0700

    Add files via upload

---
+++ Main.lua
@@ -0,0 +1,11 @@
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	--Blox Fruits
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/BloxFruits.lua"))()
+elseif placeId == 205224386 then
+	--HideAndSeek
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/HideAndSeek.lua"))()
+elseif placeId == 116495829188952 then
+    	--DeadRails
+    	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/DeadRails.lua"))()
+end
