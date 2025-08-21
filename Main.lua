cec6478615f095b64978cd36756a1dd6d8524627
Dev-BlueX <bluexscript11@gmail.com>
2025-08-21 09:15:15 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
 	--Blox Fruits
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/BloxFruits.luau"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/main/BloxFruits.lua"))()
 elseif placeId == 205224386 then
 	--HideAndSeek
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/HideAndSeek.lua"))()
