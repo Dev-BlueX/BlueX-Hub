8de6a67f8357f438c38856d0e9432dbd1c4e26bd
Dev-BlueX <bluexscript11@gmail.com>
2025-12-07 12:20:12 +0700

    Add files via upload

---
+++ FarmChestVoxSeas.lua
@@ -0,0 +1,2 @@
+script_key = "KEY"; -- A key might be required, if not, delete this line.
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/5527918b9edb609265199b3901a4d389.lua"))()
