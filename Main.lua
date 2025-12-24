ebb2c5dad17cf96e99aa624982ba7ccec4d25aed
Dev-BlueX <bluexscript11@gmail.com>
2025-12-24 12:29:15 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 _G.ThemeID = _G.ThemeID or nil
 local placeId = game.PlaceId
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 or placeId == 122478697296975 or UniverseID == 994732206 then
+if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 or placeId == 122478697296975 or placeId == 73902483975735 or UniverseID == 994732206 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/...../main/BloxFruits.lua"))()
 elseif placeId == 205224386 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/HideAndSeek.lua"))()
